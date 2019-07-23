e443dea29937ebd01b44c6f00c21cf14a98770e2
webkey <serhiy_i@ukr.net>
2019-07-23 19:40:08 +0300

    Правки по print.scss, переделан скрипт состояний полей ввода

--- src/js/common.js
+++ src/js/common.js
@@ -24,78 +24,31 @@ function placeholderInit() {
 }
 
 /**
- * !Toggle class on a form's element on focus
+ * !Add classes to form elements
+ * if they has a value or they are in focus
  * */
-function inputFocusClass() {
-  var $inputs = $('.field-js');
-
-  if ($inputs.length) {
-    var $fieldWrap = $('.input-wrap');
-    var $selectWrap = $('.select');
-    var classFocus = 'focused';
-
-    $inputs.focus(function () {
-      var $currentField = $(this);
-      var $currentFieldWrap = $currentField.closest($fieldWrap);
-
-      $currentField.addClass(classFocus);
-      $currentField.prev('label').addClass(classFocus);
-      $currentField.closest($selectWrap).prev('label').addClass(classFocus);
-      $currentFieldWrap.addClass(classFocus);
-      $currentFieldWrap.find('label').addClass(classFocus);
-
-    }).blur(function () {
-      var $currentField = $(this);
-      var $currentFieldWrap = $currentField.closest($fieldWrap);
-
-      $currentField.removeClass(classFocus);
-      $currentField.prev('label').removeClass(classFocus);
-      $currentField.closest($selectWrap).prev('label').removeClass(classFocus);
-      $currentFieldWrap.removeClass(classFocus);
-      $currentFieldWrap.find('label').removeClass(classFocus);
-
-    });
-  }
-}
-
-/**
- * !Toggle class on a form's element if this one has a value
- * */
-function inputHasValueClass() {
-  var $inputs = $('.field-js');
-
-  if ($inputs.length) {
-    var $fieldWrap = $('.input-wrap');
-    var $selectWrap = $('.select');
-    var classHasValue = 'filled';
-
-    $.each($inputs, function () {
-      switchHasValue.call(this);
-    });
-
-    $inputs.on('keyup change', function () {
-      switchHasValue.call(this);
-    });
-
-    function switchHasValue() {
-      var $currentField = $(this);
-      var $currentFieldWrap = $currentField.closest($fieldWrap);
-
-      //first element of the select must have a value empty ("")
-      if ($currentField.val().length === 0) {
-        $currentField.removeClass(classHasValue);
-        $currentField.prev('label').removeClass(classHasValue);
-        $currentField.closest($selectWrap).prev('label').removeClass(classHasValue);
-        $currentFieldWrap.removeClass(classHasValue);
-        $currentFieldWrap.find('label').removeClass(classHasValue);
-      } else if (!$currentField.hasClass(classHasValue)) {
-        $currentField.addClass(classHasValue);
-        $currentField.prev('label').addClass(classHasValue);
-        $currentField.closest($selectWrap).prev('label').addClass(classHasValue);
-        $currentFieldWrap.addClass(classHasValue);
-        $currentFieldWrap.find('label').addClass(classHasValue);
-      }
+function formElementState() {
+  var $elem = $('.field-js');
+  
+  if ($elem.length) {
+    function toggleStateClass(mod, cond) {
+      var $this = $(this);
+      $this.add($this.prev('label')).toggleClass(mod, cond);
     }
+
+    // Focus
+    $elem.on('focus blur', function (e) {
+      toggleStateClass.call(this, 'focused', e.handleObj.origType === "focus");
+    });
+
+    // Has value
+    $.each($elem, function () {
+      toggleStateClass.call(this, 'filled', $(this).val().length !== 0);
+    });
+
+    $elem.on('keyup change', function () {
+      toggleStateClass.call(this, 'filled', $(this).val().length !== 0);
+    });
   }
 }
 
@@ -131,7 +84,7 @@ function formValidation() {
 
   if ($form.length) {
     var changeClasses = function (elem, remove, add) {
-      console.log('changeClasses');
+      // console.log('changeClasses');
       elem
           .removeClass(remove).addClass(add);
       elem
@@ -176,8 +129,7 @@ $(window).on('debouncedresize', function () {
 $(document).ready(function () {
   addTouchClasses();
   placeholderInit();
-  inputFocusClass();
-  inputHasValueClass();
+  formElementState();
   customSelect($('select.cselect'));
   objectFitImages(); // object-fit-images initial
 
